1803ffdd9c365b51da034e61fe6b2e0a3d02165a
student <student@tp.local>
2025-07-07 22:22:07 +0300

    new comit

---
+++ fourth/devsearch/projects/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from .models import Project, Tag
+from .forms import ProjectForm
+
+
+def projects(request):
+    pr = Project.objects.all()
+
+    context = {
+        'projects': pr
+    }
+    return render(request, "projects/projects.html", context)
+
+def project(request, pk):
+    project_obj = Project.objects.get(id=pk)
+
+    return render(request, 'projects/single-project.html', {'project': project_obj})
+
+def create_project(request):
+    form = ProjectForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'projects/form-template.html', context)
