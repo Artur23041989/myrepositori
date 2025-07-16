d8035e7d1df266f14c11a415a5fb1c55e14f7d5d
student <student@tp.local>
2025-07-16 22:07:07 +0300

    new comit

--- fourth/devsearch/users/views.py
+++ fourth/devsearch/users/views.py
@@ -12,4 +12,15 @@ def profiles(request):
     return render(request, 'users/index.html', context)
 
 def user_profile(request, pk):
-    return render(request, 'users/profile.html')
+    prof = Profile.objects.get(id=pk)
+
+    top_skills = prof.skill_set.exclude(description__exact='')
+    other_skills = prof.skill_set.filter(description='')
+
+    context = {
+        "profile": prof,
+        "top_skills": top_skills,
+        "other_skills": other_skills
+    }
+
+    return render(request, 'users/profile.html', context)
