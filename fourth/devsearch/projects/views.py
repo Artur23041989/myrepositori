4bb0fa1908dbdf0bdfb1bffeab5eaf927bb657a8
student <student@tp.local>
2025-07-21 21:54:53 +0300

    new comit

--- fourth/devsearch/projects/views.py
+++ fourth/devsearch/projects/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Project, Tag
 from .forms import ProjectForm
+from django.contrib.auth.decorators import login_required
+
 
 
 def projects(request):
@@ -11,11 +13,13 @@ def projects(request):
     }
     return render(request, "projects/projects.html", context)
 
+@login_required(login_url='login')
 def project(request, pk):
     project_obj = Project.objects.get(id=pk)
 
     return render(request, 'projects/single-project.html', {'project': project_obj})
 
+@login_required(login_url='login')
 def create_project(request):
     form = ProjectForm()
 
