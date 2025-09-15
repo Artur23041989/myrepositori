468365ce80a4d01343ea6bd0229904c470cc7d88
student <student@tp.local>
2025-09-15 21:44:01 +0300

    new comit

--- fifth/programming_blog/blog/views.py
+++ fifth/programming_blog/blog/views.py
@@ -17,8 +17,13 @@ class BlogHome(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Главная страница'
+        context['cat_selected'] = 0
         context['menu'] = menu
         return context
+
+    def get_queryset(self):
+        return Blog.objects.filter(is_published=True).select_related('cat')
+
 
 class ShowPost(DetailView):
     model = Blog
@@ -32,16 +37,20 @@ class ShowPost(DetailView):
         context['menu'] = menu
         return context
 
-# class BlogCategory(ListView):
-#     model = Blog
-#     template_name = 'blog/index.html'
-#     context_object_name = 'posts'
-#     allow_empty = False
-#
-#
-#
-#     def get_context_data(self, *, object_list=None, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['title'] =
-#         context['menu'] = menu
-#         return context
+
+class BlogCategory(ListView):
+    model = Blog
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_queryset(self):
+        return Blog.objects.filter(cat_slug=self.kwargs['cat_slug'],
+                                   is_published=True).select_related('cat')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
+        context['cat_selected'] = context['posts'][0].cat_id
+        context['menu'] = menu
+        return context
